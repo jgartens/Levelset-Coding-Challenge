b709d49660df216a07bdd74cfe209ead6d00848c
Jacob Gartenstein <jake@Jacobs-MacBook-Pro.local>
2020-11-27 21:31:35 -0600

    updated mapping for reports

--- src/main/java/com/levelset/challenge/Controller/MainController.java
+++ src/main/java/com/levelset/challenge/Controller/MainController.java
@@ -7,6 +7,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import com.levelset.challenge.Model.Customer;
+import com.levelset.challenge.Model.Project;
+import com.levelset.challenge.Repository.ProjectRepository;
 import com.levelset.challenge.Service.ProjectService;
 
 import org.hibernate.boot.model.source.internal.hbm.CommaSeparatedStringHelper;
@@ -14,6 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -23,6 +27,10 @@ public class MainController {
     
     @Autowired
     private ProjectService projectService;
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
 
     @GetMapping("/")
     public String home(){
@@ -40,13 +48,91 @@ public class MainController {
         }
 
         //Attempt to parse file, using model for error handling
-        model = projectService.parseCSV(file, model);
+        List<Long> currentProject_ids = projectService.parseCSV(file, model);
 
         //if there's an error, return home for reprompt CSV upload
         if (model.containsAttribute("errorMsg")){
             return "home.html";
         }
-        //else, give the results page 
+        List<Project> currentProjects = projectRepository.findByIdIn(currentProject_ids);
+
+        model.addAttribute("projects", currentProjects);
+        
         return "result.html";
+    }
+
+    @GetMapping("/report/saved")
+    public String getSavedReports(Model model){
+        List<Project> allProjects = projectRepository.findAll();
+
+        model.addAttribute("projects", allProjects);
+        return "projects.html";
+    }
+
+    @GetMapping("/customer/{customer_id}")
+    public String getCustomerReport(@PathVariable(value = "customer_id") String customer_name, Model model){
+        List<Project> customer_projects = projectRepository.findByCustomerName(customer_name);
+        System.out.println(customer_projects);
+        System.out.println(customer_name);
+        if (!customer_projects.isEmpty()){
+            double totalDebt = 0;
+            int totalOrders = 0;
+            int totalProjects = 0;
+    
+            for (Project project : customer_projects){
+                totalDebt += project.getOustandingDebt();
+                totalProjects += 1;
+                if (project.getNotice() != null){
+                    totalOrders+=1;
+                }
+                if (project.getLien() != null){
+                    totalOrders+=1;
+                }
+            }
+
+            Customer customer = new Customer(customer_name, totalDebt, totalOrders, totalProjects);
+            model.addAttribute("customer", customer);
+            return "customer.html";
+        }
+
+        else{
+
+            model.addAttribute("errorMsg", "Customer was not found, please try again");
+            return "projects.html";
+        }  
+    }
+
+
+    @PostMapping("/customer/{customer_id}")
+    public String getCustomerReportFromName(@RequestParam("customer") String customer_name, Model model){
+        List<Project> customer_projects = projectRepository.findByCustomerName(customer_name);
+        System.out.println(customer_projects);
+        System.out.println("HERE THO");
+        if (!customer_projects.isEmpty()){
+            double totalDebt = 0;
+            int totalOrders = 0;
+            int totalProjects = 0;
+    
+            for (Project project : customer_projects){
+                totalDebt += project.getOustandingDebt();
+                totalProjects += 1;
+                if (project.getNotice() != null){
+                    totalOrders+=1;
+                }
+                if (project.getLien() != null){
+                    totalOrders+=1;
+                }
+            }
+
+            Customer customer = new Customer(customer_name, totalDebt, totalOrders, totalProjects);
+            model.addAttribute("customer", customer);
+            return "customer.html";
+        }
+
+        else{
+
+            model.addAttribute("errorMsg", "Customer was not found, please try again");
+            return "projects.html";
+        }  
     }
 }
