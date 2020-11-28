f16bdd910a09045653b35b6c6b99f302f3c28c61
Jacob Gartenstein <jake@Jacobs-MacBook-Pro.local>
2020-11-27 21:32:26 -0600

    bug fix findall -> find

--- src/main/java/com/levelset/challenge/Repository/ProjectRepository.java
+++ src/main/java/com/levelset/challenge/Repository/ProjectRepository.java
@@ -1,5 +1,6 @@
 package com.levelset.challenge.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.levelset.challenge.Model.Project;
@@ -9,5 +10,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProjectRepository extends CrudRepository<Project, Long> {
+    List<Project> findAll();
     Optional<Project> findById(Long id);
+    List<Project> findByIdIn(List<Long> ids); 
+    List<Project> findByCustomerName(String customerName);
 }
